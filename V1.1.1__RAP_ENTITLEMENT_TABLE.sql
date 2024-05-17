d5ab2a75dbf8c5e284679528677f4f9b187a8966
Nishikanta Bhadra <nishikanta.bhadra@snowflake.com>
2024-05-17 12:42:32 -0400

    Add files via upload

---
+++ V1.1.1__RAP_ENTITLEMENT_TABLE.sql
@@ -0,0 +1,14 @@
+
+--	==================================================================================================================
+--  Purpose: Row Access Policy entitlement table to provide row level access based on Roles assigned to a SCOA code
+--
+--  Revision History:
+--  Date     		Engineer      Description
+--  04/15/2024					  Initial table creation script
+--	==================================================================================================================
+
+CREATE SCHEMA cicd_demo;
+CREATE TABLE IF NOT EXISTS rap_entitlement_table_cicd (
+ROLE        VARCHAR(100),
+SCOA_CODE   VARCHAR(100)
+);
