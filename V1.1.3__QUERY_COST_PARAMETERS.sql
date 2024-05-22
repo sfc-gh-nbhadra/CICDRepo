d26d27822a77e152f6d0686c5033a6b4b2b71dae
Nishi Bhadra <nishikanta.bhadra@snowflake.com>
2024-05-22 12:23:12 -0400

    test
    test

---
+++ V1.1.3__QUERY_COST_PARAMETERS.sql
@@ -0,0 +1,15 @@
+--	======================================================================================
+
+--  Purpose: 	Create QUERY_COST_PARAMETERS table to capture begining timestamp of the 
+--				window to calculate    
+--
+--  Revision History:
+--  Date     		Engineer      Description
+--  04/15/2024					  Initial table creation script 
+
+--	======================================================================================
+
+CREATE TABLE IF NOT EXISTS QUERY_COST_PARAMETERS (
+	PARAMETER_NAME 			VARCHAR (100),
+ 	PARAM_VALUE_TIMESTAMP 	TIMESTAMP_LTZ,
+ 	PARAM_VALUE_STRING 		VARCHAR (100));       
